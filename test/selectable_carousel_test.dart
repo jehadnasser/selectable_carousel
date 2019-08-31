c24a6325e108e3abc3c461efcf1472fe4a71ee8c
jehadhnasser@gmail.com <jehadhnasser@gmail.com>
2019-08-31 16:01:50 +0200

    Add Test

--- test/selectable_carousel_test.dart
+++ test/selectable_carousel_test.dart
@@ -1,7 +1,7 @@
-//import 'package:flutter_test/flutter_test.dart';
-//
-////void main() {
-////  test('Runs fine!', () {
-////
-////  });
-////}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Runs fine!', () {
+    print('Nothing to test yet!')
+  });
+}
